35db41ddd0ebf1adbb5b6a9f7f77a652c65ef71d
Lautaro Elizondo <84641594+Lautaro-07@users.noreply.github.com>
2025-12-05 11:29:33 -0300

    Delete cliente directory

--- cliente/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['portal_client_id']);
-unset($_SESSION['portal_client_name']);
-unset($_SESSION['portal_client_email']);
-header('Location: ../login.php');
-exit;
